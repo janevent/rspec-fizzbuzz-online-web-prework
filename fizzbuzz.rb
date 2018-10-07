a1d22c256fd71f2ed145f69d0fbbb962d8cdfe15
Jane Venter <janeanneventer@gmail.com>
2018-10-07 23:11:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,8 @@ def fizzbuzz(num)
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
+  elsif num % 5 == 0 && num % 3 == 0
+    "FizzBuzz"
 end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
